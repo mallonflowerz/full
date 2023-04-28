63a19b4a341305a95c2317f7230681a5e395891b
marlonflowers <113075212+marlonflowers@users.noreply.github.com>
2023-04-28 10:04:26 -0500

    1.3

--- src/main/java/com/apirest/full/controller/faseFinalControllers/RoundOf16Controller.java
+++ src/main/java/com/apirest/full/controller/faseFinalControllers/RoundOf16Controller.java
@@ -1,9 +1,11 @@
 package com.apirest.full.controller.faseFinalControllers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,28 +45,117 @@ public class RoundOf16Controller {
         return roundOf16Services.obtenerTodo();
     }
 
-    // @GetMapping("/saveRound")
-    // public List<RoundOf16> guardarRonda() {
-    //     Map<String, List<? extends Equipo>> grupos = new HashMap<>();
-    //     grupos.put("Grupo A", grupoAService.topTwo());
-    //     grupos.put("Grupo B", grupoBService.topTwo());
-    //     grupos.put("Grupo C", grupoCService.topTwo());
-    //     grupos.put("Grupo D", grupoDService.topTwo());
-    //     // Agrega aquí tantos grupos como quieras
+    @GetMapping("/count")
+    public Long cantidadData(){
+        return roundOf16Services.cantidad();
+    }
 
-    //     List<RoundOf16> listaRound = new ArrayList<>();
+    @GetMapping("/reset")
+    public List<RoundOf16> resetearResult(){
+        List<RoundOf16> roundAll = roundOf16Services.obtenerTodo();
+        //List<RoundOf16> lista = new ArrayList<>();
 
-    //     for (String nombreGrupo : grupos.keySet()) {
-    //         List<? extends Equipo> listaEquipo = grupos.get(nombreGrupo);
+        for (RoundOf16 rOf16: roundAll){
+            RoundOf16 roundOf16 = new RoundOf16();
+            roundOf16.setId(rOf16.getId());
+            roundOf16.setEquipo1(rOf16.getEquipo1());
+            roundOf16.setEquipo2(rOf16.getEquipo2());
+            roundOf16.setResulEquipo1(0);
+            roundOf16.setResulEquipo2(0);
+            roundOf16Services.guardar(roundOf16);
+            //lista.add(roundOf16);
+        }
 
-    //         RoundOf16 roundOf16 = new RoundOf16();
-    //         roundOf16.setEquipo1(listaEquipo.get(0).getNombre());
-    //         roundOf16.setEquipo2(listaEquipo.get(1).getNombre());
-    //         roundOf16.setResulEquipo1(0);
-    //         roundOf16.setResulEquipo2(0);
-    //         // roundOf16Services.guardar(roundOf16);
-    //         listaRound.add(roundOf16);
-    //     }
-    //     return listaRound;
-    // }
+        return roundOf16Services.obtenerTodo();
+    }
+
+    @GetMapping("/saveRound")
+    public List<RoundOf16> guardarRonda() {
+        List<RoundOf16> roundAll = roundOf16Services.obtenerTodo();
+        List<GrupoAModel> listaGrupoA = grupoAService.topTwo();
+        List<GrupoBModel> listaGrupoB = grupoBService.topTwo();
+        List<GrupoCModel> listaGrupoC = grupoCService.topTwo();
+        List<GrupoDModel> listaGrupoD = grupoDService.topTwo();
+        List<String> listaName = new ArrayList<>();
+        //List<RoundOf16> listaRound = new ArrayList<>();
+        int cont = 0;
+
+        if (!listaGrupoA.isEmpty() && listaGrupoA.size() == 2) {
+            listaName.add(listaGrupoA.get(0).getNombre());
+            listaName.add(listaGrupoA.get(1).getNombre());
+        }
+        if (!listaGrupoB.isEmpty() && listaGrupoB.size() == 2) {
+            listaName.add(listaGrupoB.get(0).getNombre());
+            listaName.add(listaGrupoB.get(1).getNombre());
+        }
+        if (!listaGrupoC.isEmpty() && listaGrupoC.size() == 2) {
+            listaName.add(listaGrupoC.get(0).getNombre());
+            listaName.add(listaGrupoC.get(1).getNombre());
+        }
+        if (!listaGrupoD.isEmpty() && listaGrupoD.size() == 2) {
+            listaName.add(listaGrupoD.get(0).getNombre());
+            listaName.add(listaGrupoD.get(1).getNombre());
+        }
+
+        Collections.shuffle(listaName);
+
+        for (int i = 0; i < listaName.size() - 1; i += 2) {
+            RoundOf16 roundOf16 = new RoundOf16();
+            if (!roundAll.isEmpty()){
+                roundOf16.setId(roundAll.get(cont).getId());
+            }
+            roundOf16.setEquipo1(listaName.get(i));
+            roundOf16.setEquipo2(listaName.get(i + 1));
+            roundOf16.setResulEquipo1(0);
+            roundOf16.setResulEquipo2(0);
+            roundOf16Services.guardar(roundOf16);
+            //listaRound.add(roundOf16);
+            cont +=1;
+        }
+
+        return roundOf16Services.obtenerTodo();
+    }
+
+    @PostMapping("/result/{min}/{max}")
+    public List<RoundOf16> generarResultados(@PathVariable("min") int min, @PathVariable("max") int max){
+        List<RoundOf16> roundAll = roundOf16Services.obtenerTodo();
+        //List<RoundOf16> lista = new ArrayList<>();
+
+        for (RoundOf16 rOf16: roundAll){
+            Random random = new Random();
+            int[] result = new int[2];
+            RoundOf16 roundOf16 = new RoundOf16();
+            result[0] = random.nextInt(max - min + 1) + min;
+            result[1] = random.nextInt(max - min + 1) + min;
+            roundOf16.setId(rOf16.getId());
+            roundOf16.setEquipo1(rOf16.getEquipo1());
+            roundOf16.setEquipo2(rOf16.getEquipo2());
+            roundOf16.setResulEquipo1(result[0]);
+            roundOf16.setResulEquipo2(result[1]);
+            roundOf16Services.guardar(roundOf16);
+            //lista.add(roundOf16);
+        }
+
+        return roundOf16Services.obtenerTodo();
+    }
+
+    @DeleteMapping("/deleteForId/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id) {
+        boolean ok = roundOf16Services.eliminarItem(id);
+        if (ok) {
+            return "Se ha eliminado el id: " + id + " correctamente";
+        } else {
+            return "Algo salio mal";
+        }
+    }
+
+    @DeleteMapping("/deleteAll")
+    public String eliminarTodo(){
+        boolean ok = roundOf16Services.eliminarTodo();
+        if (ok) {
+            return "Eliminado todo correctamente";
+        } else {
+            return "Algo salio mal";
+        }
+    }
 }
--- src/main/java/com/apirest/full/service/faseFinalServices/RoundOf16Services.java
+++ src/main/java/com/apirest/full/service/faseFinalServices/RoundOf16Services.java
@@ -22,4 +22,26 @@ public class RoundOf16Services {
     public List<RoundOf16> obtenerTodo(){
         return roundOf16Repo.findAll();
     }
+
+    public Long cantidad(){
+        return roundOf16Repo.count();
+    }
+
+    public boolean eliminarItem(Long id){
+        try {
+            roundOf16Repo.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean eliminarTodo(){
+        try {
+            roundOf16Repo.deleteAll();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
